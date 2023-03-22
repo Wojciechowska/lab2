8105e122c80c3bd4b4cd2644006349538c4627f9
Wojciechowska <aga_woj8@wp.pl>
2023-03-22 12:23:57 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -62,6 +62,48 @@ ilosc_elementow = len(gry)
 print(ilosc_elementow)
 
 #zad10
+print('Wpisz zdanie.')
+zdanie = input()
+print(zdanie + ' zawiera literę "a" ' + str(zdanie.count('a')) + ' razy.')
+
+
+#zad11
+liczba_pierwsza = input()
+liczba_druga = input()
+liczba_trzecia = input()
+
+liczba_pierwsza = int(liczba_pierwsza)
+liczba_druga = int(liczba_druga)
+liczba_trzecia = int(liczba_trzecia)
+
+if (liczba_pierwsza > liczba_druga) & (liczba_pierwsza > liczba_trzecia):
+    print(str(liczba_pierwsza) + ' jest największa')
+elif(liczba_druga > liczba_pierwsza) & (liczba_druga > liczba_trzecia):
+    print(str(liczba_druga) + ' jest największa')
+else:
+    print(str(liczba_trzecia) + ' jest największa')
+
+
+#zad12
+lista_kwadrat = [2, 4, 12, 10.5, 50.32, 8.88]
+print(lista_kwadrat)
+for z in range(len(lista_kwadrat)):
+    lista_kwadrat[z] = lista_kwadrat[z] ** 2
+print(lista_kwadrat)
+
+
+#zad13
+print('Podaj 10 liczb')
+liczby = []
+i = 1
+while i <= 10:
+    m = int(input())
+    if m % 2 == 0:
+        liczby.append(m)
+    i += 1
+print(liczby)
+
+
 
 
 
